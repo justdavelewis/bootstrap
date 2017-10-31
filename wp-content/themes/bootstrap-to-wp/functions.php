f3c56374e9b7f1be631f4fd331816250808454c2
StyleChild <david@madebyfactory.com>
2017-10-31 11:05:13 +0000

    adding pages

--- wp-content/themes/bootstrap-to-wp/functions.php
+++ wp-content/themes/bootstrap-to-wp/functions.php
@@ -1,37 +1,35 @@
 <?php
 
-function theme_styles() {
+	function theme_styles() {
 
-	wp_enqueue_style('bootstrap_css', get_template_directory_uri() . '/css/bootstrap.min.css');
-	wp_enqueue_style('theme_css', get_template_directory_uri() . '/style.css');
+		wp_enqueue_style('bootstrap_css', get_template_directory_uri() . '/css/bootstrap.min.css');
+		wp_enqueue_style('theme_css', get_template_directory_uri() . '/style.css');
 
-}
-add_action('wp_enqueue_scripts', 'theme_styles');
+	}
+	add_action('wp_enqueue_scripts', 'theme_styles');
 
-function theme_js() {
+	function theme_js() {
 
-	global $wp_scripts;
+		global $wp_scripts;
 
-	wp_register_script('html5_shiv', 'https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js', '', '', false);
-	wp_register_script('respond_js', 'https://oss.maxcdn.com/respond/1.4.2/respond.min.js', '', '', false);
+		wp_register_script('html5_shiv', 'https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js', '', '', false);
+		wp_register_script('respond_js', 'https://oss.maxcdn.com/respond/1.4.2/respond.min.js', '', '', false);
 
-	$wp_scripts-> add_data( 'html5_shiv', 'conditional', 'lt IE 9');
-	$wp_scripts-> add_data( 'respond_js', 'conditional', 'lt IE 9');
+		$wp_scripts-> add_data( 'html5_shiv', 'conditional', 'lt IE 9');
+		$wp_scripts-> add_data( 'respond_js', 'conditional', 'lt IE 9');
 
-	wp_enqueue_script('bootstrap_js', get_template_directory_uri() . '/js/bootstrap.min.js', array('jquery'), '', true);
+		wp_enqueue_script('bootstrap_js', get_template_directory_uri() . '/js/bootstrap.min.js', array('jquery'), '', true);
+	}
+	add_action('wp_enqueue_scripts', 'theme_js');
 
+	add_theme_support('menus');
 
-}
-add_action('wp_enqueue_scripts', 'theme_js');
-
-add_theme_support('menus');
-
-function register_theme_menus() { 
-	register_nav_menus(
-		array(
-			'header-menu' => __( 'Header Menu' )  // make sure to put in a double _ -> __//
-		) 
-	);
-}
-add_action('init', 'register_theme_menus');
+	function register_theme_menus() { 
+		register_nav_menus(
+			array(
+				'header-menu' => __( 'Header Menu' )  // make sure to put in a double _ -> __//
+			)
+		);
+	}
+	add_action('init', 'register_theme_menus');
 ?>
