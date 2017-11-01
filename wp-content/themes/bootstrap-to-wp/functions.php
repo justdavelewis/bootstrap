1ababccba30cdffef64c39bc4899822aca6821fe
StyleChild <david@madebyfactory.com>
2017-11-01 12:21:33 +0000

    more shiz

--- wp-content/themes/bootstrap-to-wp/functions.php
+++ wp-content/themes/bootstrap-to-wp/functions.php
@@ -1,35 +1,55 @@
 <?php
 
-	function theme_styles() {
+function theme_styles() {
 
-		wp_enqueue_style('bootstrap_css', get_template_directory_uri() . '/css/bootstrap.min.css');
-		wp_enqueue_style('theme_css', get_template_directory_uri() . '/style.css');
+	wp_enqueue_style('bootstrap_css', get_template_directory_uri() . '/css/bootstrap.min.css');
+	wp_enqueue_style('theme_css', get_template_directory_uri() . '/style.css');
 
-	}
-	add_action('wp_enqueue_scripts', 'theme_styles');
+}
+add_action('wp_enqueue_scripts', 'theme_styles');
 
-	function theme_js() {
+function theme_js() {
 
-		global $wp_scripts;
+	global $wp_scripts;
 
-		wp_register_script('html5_shiv', 'https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js', '', '', false);
-		wp_register_script('respond_js', 'https://oss.maxcdn.com/respond/1.4.2/respond.min.js', '', '', false);
+	wp_register_script('html5_shiv', 'https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js', '', '', false);
+	wp_register_script('respond_js', 'https://oss.maxcdn.com/respond/1.4.2/respond.min.js', '', '', false);
 
-		$wp_scripts-> add_data( 'html5_shiv', 'conditional', 'lt IE 9');
-		$wp_scripts-> add_data( 'respond_js', 'conditional', 'lt IE 9');
+	$wp_scripts-> add_data( 'html5_shiv', 'conditional', 'lt IE 9');
+	$wp_scripts-> add_data( 'respond_js', 'conditional', 'lt IE 9');
 
-		wp_enqueue_script('bootstrap_js', get_template_directory_uri() . '/js/bootstrap.min.js', array('jquery'), '', true);
-	}
-	add_action('wp_enqueue_scripts', 'theme_js');
+	wp_enqueue_script('bootstrap_js', get_template_directory_uri() . '/js/bootstrap.min.js', array('jquery'), '', true);
+}
+add_action('wp_enqueue_scripts', 'theme_js');
 
-	add_theme_support('menus');
+//add_filter( 'show_admin_bar', '__return_false' );
 
-	function register_theme_menus() { 
-		register_nav_menus(
-			array(
-				'header-menu' => __( 'Header Menu' )  // make sure to put in a double _ -> __//
-			)
-		);
-	}
-	add_action('init', 'register_theme_menus');
+add_theme_support('menus');
+
+function register_theme_menus() { 
+	register_nav_menus(
+		array(
+			'header-menu' => __( 'Header Menu' )  // make sure to put in a double _ -> __//
+		)
+	);
+}
+add_action('init', 'register_theme_menus');
+
+function create_widget( $name, $id, $description ) {
+
+	register_sidebar(array(
+		'name' => __( $name ),	 
+		'id' => $id, 
+		'description' => __( $description ),
+		'before_widget' => '<div class="widget">',
+		'after_widget' => '</div>',
+		'before_title' => '<h3>',
+		'after_title' => '</h3>'
+	));
+
+}
+create_widget( 'Front Page Left', 'front-left', 'Displays on the left of the homepage' );
+create_widget( 'Front Page Center', 'front-center', 'Displays in the center of the homepage' );
+create_widget( 'Front Page Right', 'front-right', 'Displays on the right of the homepage' );
+create_widget( 'Page Sidebar', 'page', 'Displays on the sides of pages that have a sidebar' );
 ?>
---
+++ wp-content/themes/bootstrap-to-wp/page-full-width.php
@@ -0,0 +1,41 @@
+<?php 
+/*
+  Template Name: Full Width Template
+*/
+?>
+
+<?php get_header(); ?>    
+
+<!-- Main jumbotron for a primary marketing message or call to action -->
+<body>
+
+  <div class="container">
+    <!-- Example row of columns -->
+    <div class="row">
+      <div class="col-md-12">
+
+        <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+
+          <div class="page-header">
+            <h1><?php the_title(); ?></h1>
+          </div>
+
+          <?php the_content(); ?>
+
+        <?php endwhile; else: ?>
+         
+          <div class="page-header">
+            <h1>Oh No!</h1>
+          </div>
+
+          <?php the_content(); ?>
+
+          <p>No content is appearing for this page!</p>
+
+        <?php endif; ?>
+
+      </div>
+    </div>
+    
+
+<?php get_footer(); ?>
